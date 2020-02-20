ac1f38df6fc0e215e803b8f598bc9f92ac551a30
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-20 18:06:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,11 @@
 class Dog 
   attr_accessor :name
   @@all = []
-  @namearray = Array.new(0)
+  @@name = []
   
   def initialize(name)
     @name = name
-    @namearray << name
+    @@name << name
     save
   end
   
@@ -16,7 +16,7 @@ class Dog
   end
   
   def self.print_all
-    @namearray.each do |x|
+    @namearray.uniq.each do |x|
       puts x
     end
   end
